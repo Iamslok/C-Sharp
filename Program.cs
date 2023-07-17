3237d54f2d147e98f1730405aeb87f88979f6e29
Sai Lokesh <sailokeshiamslok@gmail.com>
2023-07-17 20:45:12 +0530

    basics

--- Program.cs
+++ Program.cs
@@ -28,13 +28,82 @@
 
         //userInput
 
-        Console.WriteLine("What is your age?");
+        //Console.WriteLine("What is your age?");
 
-        int yourage = Convert.ToInt32(Console.ReadLine());
+        //int yourage = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine("my age is " + yourage);
+        //Console.WriteLine("my age is " + yourage);
 
+
+        //int age = 4 ;
+        //string drink = (age > 5) ? "Drink coffe" : "drink milk";
+
+
+        // Console.WriteLine(drink);
+
+        //int day = 0;
+
+        //switch (day)
+        //{
+        //    case 1:
+        //        //function
+        //        Console.WriteLine("Monday");
+        //        break;
+        //    case 2:
+        //        Console.WriteLine("tuesday");
+        //        break;
+        //    default:
+        //        Console.WriteLine("None");
+        //        break;
+        //}
+
+
+        //int number = 0;
+
+        ////while(number <= 5)
+        ////{
+        ////    Console.WriteLine(number);
+        ////    number++;
+        ////}
+
+        //do
+        //{
+        //    Console.WriteLine(number);
+        //    number++;
+        //}
+        //while (number <= 5);
+
+        //for(int number = 0; number <=5; number++)
+        //{
+        //    Console.WriteLine(number);
+        //}
+
+        //string[] colors = { "red", "blue", "green" };
+        //colors[0] = "white";
+
+        //Console.WriteLine(colors[0]);
+
+        //foreach(string color in colors)
+        //{
+        //    Console.WriteLine(color);
+        //}
+
+        //myMethod("lokesh",0);
+
+      int a  =  myMethod(10,20);
+      double b =  myMethod(10.20,20.30);
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
     }
 
-}
+    static int myMethod(int a, int b)
+    {
+        return a + b;    
+    }
 
+    static double myMethod(double a, double b) {
+        return a + b;
+    }
+}
+ 
