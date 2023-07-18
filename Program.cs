48d40d090a5da3fb8d25650c5afa50d2e1421651
Sai Lokesh <sailokeshiamslok@gmail.com>
2023-07-18 20:42:32 +0530

    classes and objects

--- Program.cs
+++ Program.cs
@@ -1,109 +1,128 @@
-﻿class demo
+﻿using ConsoleApp1;
+
+class demo
 {
     //string name = "Lokesh";
 
-    public static void Main(string[] args)
+    //public static void Main(string[] args)
+    //{
+    //    ////implicit
+    //    //int price = 10;
+    //    //double prices = price;
+
+    //    ////explicit
+    //    //double age = 10.11;
+    //    //int myage = (int)age;
+
+    //    //Console.WriteLine(age);
+    //    //Console.WriteLine(myage);
+
+    //    //Type conversion Methods
+    //    //int age = 10;
+    //    //double ages = 10.20;
+    //    //bool isactive = true;
+
+    //    //Console.WriteLine(Convert.ToString(age)); //10 "10"
+    //    //Console.WriteLine(Convert.ToDouble(age));
+    //    //Console.WriteLine(Convert.ToInt32(ages));
+    //    //Console.WriteLine(Convert.ToString(isactive));  
+    //    //Console.WriteLine(isactive);  
+
+    //    //userInput
+
+    //    //Console.WriteLine("What is your age?");
+
+    //    //int yourage = Convert.ToInt32(Console.ReadLine());
+
+    //    //Console.WriteLine("my age is " + yourage);
+
+
+    //    //int age = 4 ;
+    //    //string drink = (age > 5) ? "Drink coffe" : "drink milk";
+
+
+    //    // Console.WriteLine(drink);
+
+    //    //int day = 0;
+
+    //    //switch (day)
+    //    //{
+    //    //    case 1:
+    //    //        //function
+    //    //        Console.WriteLine("Monday");
+    //    //        break;
+    //    //    case 2:
+    //    //        Console.WriteLine("tuesday");
+    //    //        break;
+    //    //    default:
+    //    //        Console.WriteLine("None");
+    //    //        break;
+    //    //}
+
+
+    //    //int number = 0;
+
+    //    ////while(number <= 5)
+    //    ////{
+    //    ////    Console.WriteLine(number);
+    //    ////    number++;
+    //    ////}
+
+    //    //do
+    //    //{
+    //    //    Console.WriteLine(number);
+    //    //    number++;
+    //    //}
+    //    //while (number <= 5);
+
+    //    //for(int number = 0; number <=5; number++)
+    //    //{
+    //    //    Console.WriteLine(number);
+    //    //}
+
+    //    //string[] colors = { "red", "blue", "green" };
+    //    //colors[0] = "white";
+
+    //    //Console.WriteLine(colors[0]);
+
+    //    //foreach(string color in colors)
+    //    //{
+    //    //    Console.WriteLine(color);
+    //    //}
+
+    //    //myMethod("lokesh",0);
+
+    //  int a  =  myMethod(10,20);
+    //  double b =  myMethod(10.20,20.30);
+
+    //    Console.WriteLine(a);
+    //    Console.WriteLine(b);
+    //}
+
+    //static int myMethod(int a, int b)
+    //{
+    //    return a + b;    
+    //}
+
+    //static double myMethod(double a, double b) {
+    //    return a + b;
+    //}
+
+    static void Main(string[] args)
     {
-        ////implicit
-        //int price = 10;
-        //double prices = price;
+       table item1 = new table();
+        item1.color = "red";
+        item1.price = 100;
+        item1.name = "ford";
+       table item2 = new table();
+        item2.color = "white";
+        item2.price = 100;
+        item2.name = "kia";
 
-        ////explicit
-        //double age = 10.11;
-        //int myage = (int)age;
-
-        //Console.WriteLine(age);
-        //Console.WriteLine(myage);
-
-        //Type conversion Methods
-        //int age = 10;
-        //double ages = 10.20;
-        //bool isactive = true;
-
-        //Console.WriteLine(Convert.ToString(age)); //10 "10"
-        //Console.WriteLine(Convert.ToDouble(age));
-        //Console.WriteLine(Convert.ToInt32(ages));
-        //Console.WriteLine(Convert.ToString(isactive));  
-        //Console.WriteLine(isactive);  
-
-        //userInput
-
-        //Console.WriteLine("What is your age?");
-
-        //int yourage = Convert.ToInt32(Console.ReadLine());
-
-        //Console.WriteLine("my age is " + yourage);
-
-
-        //int age = 4 ;
-        //string drink = (age > 5) ? "Drink coffe" : "drink milk";
-
-
-        // Console.WriteLine(drink);
-
-        //int day = 0;
-
-        //switch (day)
-        //{
-        //    case 1:
-        //        //function
-        //        Console.WriteLine("Monday");
-        //        break;
-        //    case 2:
-        //        Console.WriteLine("tuesday");
-        //        break;
-        //    default:
-        //        Console.WriteLine("None");
-        //        break;
-        //}
-
-
-        //int number = 0;
-
-        ////while(number <= 5)
-        ////{
-        ////    Console.WriteLine(number);
-        ////    number++;
-        ////}
-
-        //do
-        //{
-        //    Console.WriteLine(number);
-        //    number++;
-        //}
-        //while (number <= 5);
-
-        //for(int number = 0; number <=5; number++)
-        //{
-        //    Console.WriteLine(number);
-        //}
-
-        //string[] colors = { "red", "blue", "green" };
-        //colors[0] = "white";
-
-        //Console.WriteLine(colors[0]);
-
-        //foreach(string color in colors)
-        //{
-        //    Console.WriteLine(color);
-        //}
-
-        //myMethod("lokesh",0);
-
-      int a  =  myMethod(10,20);
-      double b =  myMethod(10.20,20.30);
-
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-    }
-
-    static int myMethod(int a, int b)
-    {
-        return a + b;    
-    }
-
-    static double myMethod(double a, double b) {
-        return a + b;
+        Console.WriteLine(item2.name);
+        item1.mycars();
     }
 }
- 
+
+
+
---
+++ table.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class table
+    {
+        public table() 
+        {
+            color = "fendksjfnsdkj";
+        }
+        public string color;
+        public string name;
+        public double price;
+
+        public void mycars()
+        {
+            Console.WriteLine("dsfdsfsd");
+        }
+    }
+}
